c102c60b4dcf8f5cc3428cc590044e1654b866e5
l3miage-jaananem <mohamed-amine.jaanane@etu.univ-grenoble-alpes.fr>
2026-04-11 02:50:23 +0200

    Exo2 done

--- app/exercice2/exercice2.component.ts
+++ app/exercice2/exercice2.component.ts
@@ -8,12 +8,20 @@ import { Component, signal, computed } from '@angular/core';
 export class Exercice2Component {
 
   // TODO : Déclare un signal "estDesactive" de type boolean initialisé à true
+  protected readonly estDesactive = signal<boolean>(true);
 
 
   // TODO : Déclare un signal dérivé "texteBtn" qui retourne "Activé" ou "Désactivé"
   //         selon la valeur de estDesactive()
-
+  protected readonly texteBtn = computed<string>(() => 
+    this.estDesactive() === false ? "Activé" : "Désactivé"
+  );
 
   // TODO : Méthode "basculer()" qui inverse la valeur de estDesactive
+  protected basculer():void {
+    // Methode avec le .set() ::mais elle n'est pas appropreier pour ce cas  
+    //this.estDesactive() === true ? this.estDesactive.set(false) : this.estDesactive.set(true); 
+    this.estDesactive.update((v:boolean) => !v);
+  }
 
 }
